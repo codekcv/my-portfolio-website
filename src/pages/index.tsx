42febaf5e565a9f731550d610bbad4729a190a50
codekcv <ChristianVillamin31@gmail.com>
2020-10-28 04:02:30 +0800

    placeholder info style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,41 @@ const Index = () => (
     <Container>
       <main className="main">
         <p>
-          Christian Villamin
+          Christian Villamin's portfolio site v2.
           <br />
           &lt;This site is under construction /&gt;
         </p>
+
+        <div className="pic">
+          <p>For the mean time, here's my pic</p>
+          <img src="images/profile.jpg" alt="profile-picture" />
+        </div>
+
+        <div className="social-links">
+          <a
+            href="https://github.com/codekcv"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            GitHub
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/codekcv/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            LinkedIn
+          </a>
+
+          <a
+            href="mailto:codekcv31@gmail.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Email
+          </a>
+        </div>
 
         {/* <ul className="social-links">
           {socials.map((social) => (
@@ -51,6 +82,29 @@ const Container = styled.div(
       p {
         text-align: center;
         color: ${theme["text-primary"].light};
+      }
+    }
+
+    .pic {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        border-radius: 50%;
+        width: 200px;
+        height: 200px;
+        margin-bottom: 1rem;
+      }
+    }
+
+    .social-links {
+      background: rgba(255, 255, 255, 0.5);
+      border-radius: 0.5rem;
+
+      a {
+        margin: 0 0.25rem;
       }
     }
   `
